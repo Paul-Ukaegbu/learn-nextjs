af985740ca19d33bfd09c90763df45212f2f3e9a
DevPapi <paulukaegbu@devpapi.io>
2024-01-30 01:41:31 -0600

    Added analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import '@/app/ui/global.css';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 import { inter, lusitana } from '@/app/ui/fonts';
 
 export default function RootLayout({
@@ -9,6 +11,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );

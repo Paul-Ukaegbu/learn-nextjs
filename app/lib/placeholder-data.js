f68b65ebd8cff8ac59eb7b82618af240b72fa33d
DevPapi <paulukaegbu@devpapi.io>
2024-01-30 02:28:58 -0600

    added user to database

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -4,7 +4,7 @@ const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
     name: 'Paul',
-    email: 'user@nextmail.com',
+    email: 'paul@nextmail.com',
     password: '123456',
   },
 ];
